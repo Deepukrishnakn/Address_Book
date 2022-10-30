e19b54126e1726fe4c9ff32a28a70c90bf9e1a7d
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-30 20:48:39 +0530

    complited

--- addressbook/accounts/views.py
+++ addressbook/accounts/views.py
@@ -11,26 +11,71 @@ from .models import Account, UserToken
 from .serializers import RegisterSerializer
 
 
+# @api_view(['POST'])
+# def registeruser(request):
+#     data = request.data
+#     print(data)
+#     try:
+#         print('rajaaaa')
+#         user = Account.objects.create(
+#             first_name=data['first_name'],
+#             last_name=data['last_name'],
+#             email=data['email'],
+#             phone_number=data['phone_number'],
+#             password=make_password(data['password'])
+#         )
+        
+#         serializer = RegisterSerializer(user,many=False)
+#         return Response(serializer.data)
+#     except :
+#         message = {'detail':'User with this email already exist'}
+#         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['POST'])
 def registeruser(request):
-    data = request.data
-    print(data)
-    try:
-        print('rajaaaa')
-        user = Account.objects.create(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            email=data['email'],
-            phone_number=data['phone_number'],
-            password=make_password(data['password'])
-        )
-        
-        serializer = RegisterSerializer(user,many=False)
-        return Response(serializer.data)
-    except :
-        message = {'detail':'User with this email already exist'}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+# try:
+    data=request.data
+    first_name=data['first_name']
+    last_name=data['last_name']
+    email=data['email']
+    phone_number=data['phone_number'],
+    password=data['password']
+    confirm_password=data['confirm_password']
 
+    # validatations for blank
+    if email=='' or last_name=='' or first_name ==''  or password=='' or confirm_password=='':
+        message={'error':' fill the blanks'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+    # validation for password matching
+    elif password!=confirm_password:
+        message={'error':'password miss match'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+    # for password length check
+    elif len(password)<6:
+        message={'error':'password contain min 6 charector'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+    
+    # checking the email is already exist or not
+    elif Account.objects.filter(email=email).exists():
+        message={'error':'This email is already exist'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+        
+    # creating a object of Account model for signup 
+    user=Account.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        phone_number=phone_number,
+        password=make_password(password),                    
+    )
+    serializere=RegisterSerializer(user,many=False)
+    return Response(serializere.data)
+# except:
+    message={'error':'there is a error occure'}
+    return Response(message,status=status.HTTP_400_BAD_REQUEST)
+
+# user login
 class LoginAPIView(APIView):
     def post(self, request):
         email = request.data['email']
@@ -78,7 +123,7 @@ class LoginAPIView(APIView):
             }
             return response  
 
-
+# jwt refresh token
 class RefreshAPIView(APIView):
     def post(self, request):
         refresh_token = request.COOKIES.get('refresh_token')
@@ -97,12 +142,13 @@ class RefreshAPIView(APIView):
             'token':access_token
         })
 
-
+#user data view by user
 class UserAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     def get(self,request):
         return Response(RegisterSerializer(request.user).data)
-
+        
+# user logout
 class LogoutAPIView(APIView):
     def post(self, request):
         refresh_token=request.COOKIES.get('refresh_token')
